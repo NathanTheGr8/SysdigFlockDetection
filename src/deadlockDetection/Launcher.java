2457ea5cdf59cd94df7048e103146b465666c7ee
Nathan Davis <NathanTheGreat94@gmail.com>
2016-02-20 15:32:42 -0600

    main.readFile(args[0])

--- src/deadlockDetection/Launcher.java
+++ src/deadlockDetection/Launcher.java
@@ -3,6 +3,5 @@ package deadlockDetection;
 public class Launcher {
 	public static void main(String[] args) {
 		Controller main = new Controller();
-		main.readFile("/Users/liwingyee/Documents/eclipse/workspace/deadlockDetection/src/main/resources/logRaw_SH_DeadLock_ShEx.json");
-	}
+		main.readFile(args[0]);}
 }
