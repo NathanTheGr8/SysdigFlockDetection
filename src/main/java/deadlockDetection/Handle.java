3c527254b6070b56e94d16a113c36a0a49323c7d
Nathan Davis <NathanTheGreat94@gmail.com>
2016-02-20 21:30:53 -0600

    Handle set and get

--- src/main/java/deadlockDetection/Handle.java
+++ src/main/java/deadlockDetection/Handle.java
@@ -10,4 +10,24 @@ public class Handle {
 		this.id = id;
 		refCount++;
 	}
+
+	public String getFileName (){
+		return fileName;
+	}
+
+	public int getID(){
+		return id;
+	}
+
+	public int getRefCount (){
+		return refCount;
+	}
+
+	public void incrementRefCount(){
+		refCount++;
+	}
+
+	public void decrementRefCount(){
+		refCount--;
+	}
 }
