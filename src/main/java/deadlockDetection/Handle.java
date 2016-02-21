4f275ef83a638321f380f42b0105618c233550d5
Nathan <NathanTheGreat94@gmail.com>
2016-02-20 23:16:30 -0600

    small testcase mod in format

--- src/main/java/deadlockDetection/Handle.java
+++ src/main/java/deadlockDetection/Handle.java
@@ -10,4 +10,31 @@ public class Handle {
 		this.id = id;
 		refCount++;
 	}
+
+	public String getFileName (){
+		return fileName;
+	}
+
+	public int getID(){
+		return id;
+	}
+
+	public int getRefCount (){
+		return refCount;
+	}
+
+	public int incrementRefCount(){
+		refCount++;
+		return refCount;
+	}
+
+	public int decrementRefCount(){
+		
+		refCount--;
+		if (refCount < 0) {
+			throw new negitiveRefCount("negitive refCount is not valid");
+		}
+
+		return refCount;
+	}
 }
