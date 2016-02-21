fb9eefabedc4b6a8702fcfcc344af0b2f0f163d9
Nathan Davis <NathanTheGreat94@gmail.com>
2016-02-20 23:08:57 -0600

    deleted old files, new json format

--- src/deadlockDetection/Launcher.java
+++
@@ -1,8 +0,0 @@
-package deadlockDetection;
-
-public class Launcher {
-	public static void main(String[] args) {
-		Controller main = new Controller();
-		main.readFile(args[0]);
-	}
-}
--- src/deadlockDetection/LockState.java
+++
@@ -1,7 +0,0 @@
-package deadlockDetection;
-
-import java.util.HashSet;
-
-public class LockState {
-	private HashSet<Handle> lockset = new HashSet<Handle>();
-}
